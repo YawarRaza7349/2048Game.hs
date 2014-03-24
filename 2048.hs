0653230dc5eb5b860e1b1f4734eaf52ccd2e2e9e
Yawar Raza <YawarRaza7349@gmail.com>
2014-03-24 17:42:39 -0400

    Initial implementation (tested in WinGHCI 1.0.6/Haskell "2013.2.0.0" (whatever that means)).

---
+++ 2048.hs
@@ -0,0 +1,77 @@
+import Data.List
+import Data.Maybe
+import Data.Ratio
+import System.Random
+
+data Direction = DirUp | DirDown | DirLeft | DirRight deriving (Eq)
+
+type Tile = Maybe Int
+type Row = [Tile]
+type Board = [Row]
+
+chunks :: Int -> [a] -> [[a]]
+chunks s l | s <= 0 = undefined
+           | [] <- l = []
+           | otherwise = t : (chunks s d) where (t,d) = splitAt s l
+
+fillEmpties :: [[Int]] -> Board
+fillEmpties = map ((take 4) . (++(repeat Nothing)) . map (\x -> Just x))
+
+shiftBoardLeft :: Board -> [[Int]]
+shiftBoardLeft = map catMaybes
+
+mergeTiles :: [[Int]] -> [[Int]]
+mergeTiles = map $ concat . map ((map sum) . (chunks 2)) . group
+
+makeMoveLeft :: Board -> Board
+makeMoveLeft = fillEmpties . mergeTiles . shiftBoardLeft
+
+makeMove :: Direction -> Board -> Board
+makeMove DirLeft = makeMoveLeft
+makeMove DirRight = (map reverse) . makeMoveLeft . (map reverse)
+makeMove DirUp = transpose . makeMoveLeft . transpose
+makeMove DirDown = reverse . (makeMove DirUp) . reverse
+
+possibleUnitBoards :: Int -> [Board]
+possibleUnitBoards i = map (chunks 4) $ zipWith (++) (inits l) (map ((Just i):) $ tails l) where l = replicate 15 Nothing
+
+overlapBoards :: Board -> Board -> Board
+overlapBoards = zipWith $ zipWith $ (\f s -> listToMaybe $ take 1 $ catMaybes [f,s])
+
+possibleMoves :: [(Int, Board)]
+possibleMoves = zip (repeat 9) (possibleUnitBoards 2) ++ zip (repeat 1) (possibleUnitBoards 4)
+
+possibleNextMoves :: Board -> [(Int, Board)]
+possibleNextMoves b = zip is $ map (overlapBoards b) bs where (is, bs) = unzip possibleMoves
+
+possibleNextMovesNonRepeat :: Board -> [(Int, Board)]
+possibleNextMovesNonRepeat b = filter ((/= b) . snd) $ possibleNextMoves b
+
+weightsToCondProb :: [Int] -> [(Int, Int)]
+weightsToCondProb ws = zipWith (,) ws $ map sum $ tails ws
+
+chooseRandomNextBoardHelper :: StdGen -> [((Int, Int), Board)] -> (Board, StdGen)
+chooseRandomNextBoardHelper g (((n, d), b):pbs) = if r < n then (b, ng) else chooseRandomNextBoardHelper g pbs where (r, ng) = randomR (0, d-1) g
+
+chooseRandomNextBoard :: StdGen -> [(Int, Board)] -> (Board, StdGen)
+chooseRandomNextBoard g wbs = chooseRandomNextBoardHelper g $ zip (weightsToCondProb ws) bs where (ws, bs) = unzip wbs
+
+randomMove :: (Board, StdGen) -> (Board, StdGen)
+randomMove p@(b, g) = if nxt == [] then p else chooseRandomNextBoard g nxt where nxt = possibleNextMovesNonRepeat b
+
+movableBoard :: Board -> Bool
+movableBoard b = any (any isNothing . concat) $ map (flip makeMove b) [DirUp, DirLeft]
+
+pureGame :: StdGen -> [Direction] -> [Board]
+pureGame gn ds = map head $ group $ e ++ (take 1 f) where (e, f) = span movableBoard $ map (fst . ($ (randomMove $ randomMove (replicate 4 $ replicate 4 Nothing, gn)))) (scanl (flip (.)) id $ map (\d o@(b, g) -> let nxt = makeMove d b in if nxt == b then o else randomMove (nxt, g)) ds)
+
+showBoard :: Board -> String
+showBoard b = intercalate "\n" $ map (intercalate " ") $ transpose $ map (\c -> map (reverse . take (maximum (map length c)) . (++(repeat ' ')) . reverse) c) ns where ns = transpose $ map (map (\x -> case x of Just y -> show y; Nothing -> "_")) b
+
+readDirection :: String -> Maybe Direction
+readDirection s | s == "" = Nothing
+                | (c:cs) <- s = lookup c ((zip ['L', 'l'] $ repeat DirLeft) ++ (zip ['R', 'r'] $ repeat DirRight) ++ (zip ['U', 'u'] $ repeat DirUp) ++ (zip ['D', 'd'] $ repeat DirDown))
+
+main :: IO ()
+main = do myGen <- getStdGen
+          interact $ concat . map ('\n':) . map (++"\n") . map showBoard . pureGame myGen . mapMaybe readDirection . lines
--- README.md
+++ README.md
@@ -2,3 +2,18 @@
 ===========
 
 My implementation of the 2048 game in Haskell.
+
+### Credits
+Original game is [2048 by Gabriele Cirulli](http://gabrielecirulli.github.io/2048/). Also, in order to keep the game as accurate as possible, I looked in the JavaScript source to find the probability distribution of the new tiles that pop up (it's `[(2, 0.9), (4, 0.1)]`, by the way).
+
+### How to Use
+If you have a Haskell interpreter, load the file into it, then call `main`. It should display the initial board. To make a move, type in any string starting with one of the matching characters as shown below, then press Enter:
+
+direction | string starts with
+-- | --
+left | `'l'` or `'L'`
+right | `'r'` or `'R'`
+up | `'u'` or `'U'`
+down | `'d'` or `'D'`
+
+If you type something else, the game won't continue and you will need to type a valid move. The game also won't continue if you input a move that won't change the board (like the original game does). The resulting board, including the next randomly placed tile, should print out below it. Continue until there are no more moves, after which the function will end and your interpreter should take control again. Note that your interpreter must support lazily evaluating input from `interact`/`getContents` in order for the game to function properly (which any good Haskell interpreter should); although I guess you could try giving your input all at once using [Ideone](http://ideone.com/) or something, if you're feeling lucky.
